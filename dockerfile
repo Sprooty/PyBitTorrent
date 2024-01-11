b307505567bda7286441dbb206a16079eb832d07
Sprooty <github@sprooty.com>
2024-01-11 15:57:08 +1100

    Docker

--- dockerfile
+++ dockerfile
@@ -5,15 +5,17 @@ FROM python:3.10-slim
 WORKDIR /usr/src/app
 
 # Copy the current directory contents into the container at /usr/src/app
-COPY . .
+COPY . /usr/src/app
 
 RUN apt-get update && apt-get install -y \
     build-essential \
     pkg-config \
-    libmariadb-dev
+    libmariadb-dev 
 
 # Install any needed packages specified in requirements.txt
 RUN pip install --no-cache-dir -r requirements.txt
+RUN pip install PyBitTorrent
 
 # Run your application
-CMD ["python3", "/usr/src/app/example/interator.py"]
+CMD ["python3", "/usr/src/app/examples/iterator.py"]
+
--- examples/iterator.py
+++ examples/iterator.py
@@ -14,15 +14,21 @@ torrent_directory = os.path.join(base_path, '', 'torrents')
 output_directory = os.path.join(base_path, '', 'download')
 logging.info(f"Torrent Directory is {torrent_directory}")
 logging.info(f"Output Directy is {output_directory}")
-max_instances = 20
+max_instances = 1
 
 def run_torrent_client(torrent_file, output_directory):
     # Determine the correct Python command based on the operating system
     python_command = 'python' if platform.system() == 'Windows' else 'python3'
 
+    # Dynamically get the directory where this script is located
+    script_dir = os.path.dirname(os.path.realpath(__file__))
+
+    # Correct the path to Client.py
+    client_script = os.path.join(script_dir, 'Client.py')
+
     # Build the command with the appropriate Python command
     command = [
-        python_command, 'Client.py',
+        python_command, client_script,
         '--torrent', torrent_file,
         '--output-directory', output_directory
     ]
@@ -32,6 +38,7 @@ def run_torrent_client(torrent_file, output_directory):
         return subprocess.Popen(command)  # Return the process
     except Exception as e:
         logging.error(f"Failed to start torrent client for {torrent_file}: {e}")
+
 
 def main():
     torrent_files = glob(os.path.join(torrent_directory, '*.torrent'))
